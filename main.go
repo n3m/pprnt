881254f6ea89c157ac1e799563330560b4ecb56f
Alan Enrique Maldonado Navarro <mlgalanmn88@gmail.com>
2020-08-20 18:21:02 -0500

    Final version

--- main.go
+++ main.go
@@ -12,38 +12,40 @@ import (
 func Print(data interface{}) {
 	if reflect.ValueOf(data).Kind() == reflect.Struct {
 		mapData := structs.Map(data)
-		depth := 0
+		depth := 1
 		printData(mapData, &depth)
 	} else {
-		fmt.Printf("> %+v", data)
+		fmt.Printf("%+v", data)
 	}
 
 }
 
 func printData(mapData map[string]interface{}, depth *int) {
 	depthStr := ""
+	tempDepth := *depth
 	for i := 0; i < *depth; i++ {
-		depthStr += "  "
+		depthStr += "   "
 	}
 
-	if *depth < 1 {
-		fmt.Printf("%+v{", depthStr)
+	if *depth == 1 {
+		fmt.Printf("{\n")
 	}
+
 	for key, value := range mapData {
 		switch value.(type) {
 		case map[string]interface{}:
-			fmt.Printf("%+v%+v: {\n", depthStr, key)
+			fmt.Printf("%+v\"%+v\": {\n", depthStr, key)
 			*depth++
 			printData(value.(map[string]interface{}), depth)
 			break
 		default:
-			fmt.Printf("%+v%+v: %+v\n", depthStr, key, value)
+			fmt.Printf("%+v\"%+v\": %+v\n", depthStr, key, value)
 			break
 		}
 	}
 
-	if *depth > 0 {
-		fmt.Printf(strings.Repeat("  ", *depth-1) + "}\n")
+	if tempDepth > 1 {
+		fmt.Printf(strings.Repeat("   ", tempDepth-1) + "}\n")
 	} else {
 		fmt.Printf("}\n")
 	}
