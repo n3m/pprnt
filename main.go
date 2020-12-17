45db72617bbeec9d70c0cc60688b8ce4e4ff1038
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-12-16 20:00:30 -0600

    Removed old method

--- main.go
+++ main.go
@@ -95,40 +95,6 @@ func Print(arg interface{}) {
 
 }
 
-// //TrPrint ...
-// /**/
-// func TrPrint(data interface{}) error {
-// 	errMessage := "[pprnt][Print()] > "
-
-// 	if reflect.ValueOf(data).Kind() == reflect.Struct {
-// 		mapData, err := helpers.StructToMap(data)
-// 		if err != nil {
-// 			log.Printf("%+v", errMessage+err.Error())
-// 			return errors.New(errMessage + err.Error())
-// 		}
-
-// 		depth := 1
-// 		printer.PrintData(mapData, &depth)
-
-// 	} else if reflect.ValueOf(data).Type() == reflect.TypeOf(map[string]interface{}{}) {
-// 		depth := 1
-// 		printer.PrintData(data.(map[string]interface{}), &depth)
-
-// 	} else if reflect.ValueOf(data).Type() == reflect.TypeOf(map[string]string{}) {
-// 		depth := 1
-// 		toSend := map[string]interface{}{}
-// 		for key, value := range data.(map[string]string) {
-// 			toSend[key] = value
-// 		}
-// 		printer.PrintData(toSend, &depth)
-
-// 	} else {
-// 		fmt.Printf("%+v", data)
-// 	}
-
-// 	return nil
-// }
-
 //SuperDepthMapCleaning ...
 func SuperDepthMapCleaning(curMap map[string]interface{}) (map[string]interface{}, error) {
 	return cleaner.CleanMap(curMap), nil
