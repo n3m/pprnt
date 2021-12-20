e30c320be01bb7278535a11583fd0652f34cabd2
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2021-12-19 21:04:06 -0600

    Latest changes

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/n3m/pprnt
 go 1.13
 
 require (
-	github.com/mattn/go-isatty v0.0.14
 	github.com/stretchr/testify v1.7.0
 	go.mongodb.org/mongo-driver v1.8.1
 )
--- print.go
+++ print.go
@@ -1,16 +1,10 @@
 package pprnt
 
-import (
-	"os"
-
-	"github.com/mattn/go-isatty"
-)
-
 var _state *localState = &localState{
 	Level: 0,
 }
 
-var NoColor bool = !isatty.IsTerminal(os.Stdout.Fd()) && !isatty.IsCygwinTerminal(os.Stdout.Fd())
+// var NoColor bool = !isatty.IsTerminal(os.Stdout.Fd()) && !isatty.IsCygwinTerminal(os.Stdout.Fd())
 
 func Print(arg interface{}) string {
 	_state.Reset()
